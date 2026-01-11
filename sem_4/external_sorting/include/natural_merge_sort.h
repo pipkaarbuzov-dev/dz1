88743f68ea4e6f19c3a38df723480d10dc774b4b
pipkaarbuzov-dev <pipkaarbuzov@gmail.com>
2026-01-12 02:49:26 +0300

    Add header file for natural merge sort

---
+++ sem_4/external_sorting/include/natural_merge_sort.h
@@ -0,0 +1,10 @@
+#ifndef NATURAL_MERGE_SORT_H
+#define NATURAL_MERGE_SORT_H
+
+#include <vector>
+
+template<typename T>
+std::vector<T> natural_merge_sort(const std::vector<T>& input);
+
+#include "../src/natural_merge_sort.cpp"
+#endif
