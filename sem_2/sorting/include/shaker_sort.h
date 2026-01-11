213f6f5fdbdf55d426e53e74dec53b3457dd0ed8
pipkaarbuzov-dev <pipkaarbuzov@gmail.com>
2026-01-12 02:29:36 +0300

    Add header file for shaker sort implementation

---
+++ sem_2/sorting/include/shaker_sort.h
@@ -0,0 +1,12 @@
+#ifndef SHAKER_SORT_H
+#define SHAKER_SORT_H
+
+#include <vector>
+
+// Шейкерная сортировка (двунаправленный пузырек)
+template<typename T>
+void shaker_sort(std::vector<T>& arr);
+
+#include "../src/shaker_sort.cpp"
+
+#endif
